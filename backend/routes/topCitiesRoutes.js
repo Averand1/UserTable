690aeac1a7551766a77725a92f5bfbe53e4326d4
Averand1 <leonk8867@gmail.com>
2023-04-23 22:29:04 +0530

    Average income of top cities route

--- backend/routes/topCitiesRoutes.js
+++ backend/routes/topCitiesRoutes.js
@@ -4,22 +4,28 @@ const User = require('../models/User.model');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const cities = await User.aggregate([
-    {
-      $group: {
-        _id: '$city',
-        count: { $sum: 1 },
-        avg_income: { $avg: '$income' }
+  try {
+    const cities = await User.aggregate([
+      {
+        $group: {
+          _id: '$city',
+          count: { $sum: 1 },
+          avg_income: { $avg: '$income' }
+        }
+      },
+      {
+        $sort: { count: -1 }
+      },
+      {
+        $limit: 10
       }
-    },
-    {
-      $sort: { count: -1 }
-    },
-    {
-      $limit: 10
-    }
-  ]).exec();
-  res.send(cities);
+    ]).exec();
+    res.status(200).send(cities);
+  } catch (error) {
+    res.status(404).send({message: 'Error'})
+    
+  }
+
 });
 
 module.exports = router;
